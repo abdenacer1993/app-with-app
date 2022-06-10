14907cb33c3e31df58778f89f9e03026a95a60d3
abdenacer1993 <abdenacer1993@gmail.com>
2022-06-10 12:15:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import UserList from './component/userList/UserList';
 
 function App() {
+  const [listOfUSer, setListOfUSer ] = useState([])
+
+  const  getUser = async() =>{
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setListOfUSer(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+getUser()
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<UserList listOfUSer={listOfUSer} />
   );
 }
 
